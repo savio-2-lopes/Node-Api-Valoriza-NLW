248e913380972452bce8b1e96ee06abd80f1d74f
savio-2-lopes <savioaugulopes@gmail.com>
2022-07-03 22:55:35 -0300

    :rocket: Init node express api and create route GET and Route

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,13 @@ import express from 'express'
 
 const app = express()
 const PORT = 3000
+
+app.get('/test', (request, response) => {
+  return response.send('Olá NLW')
+})
+
+app.post('/test-post', (request, response) => {
+  return response.send('Olá NLW método post')
+})
+
 app.listen(PORT, () => console.log(`server is running in ${PORT}`))
