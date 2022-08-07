0767b57f69697b18f9fd14de8ef0d2c83ba2e792
savio-2-lopes <savioaugulopes@gmail.com>
2022-08-07 00:55:49 -0300

    Apply permissions in Tags

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 
+router.post("/tags", ensureAdmin, createTagController.handle);
 router.post("/users", createUserController.handle);
-router.post("/tags", createTagController.handle);
 
 export { router };
